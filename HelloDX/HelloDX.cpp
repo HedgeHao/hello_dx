24b303515296556f69d268ab1b6b89082cddb282
hedgehao <hedgehao@gmail.com>
2021-12-30 04:50:04 -0800

    init directx 11 windows in fixed background color

---
+++ HelloDX/HelloDX.cpp
@@ -0,0 +1,185 @@
+﻿//// HelloDX.cpp : Defines the entry point for the application.
+////
+//
+//#include <Windows.h>
+//
+//#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+//// Windows Header Files
+//// C RunTime Header Files
+//#include <stdlib.h>
+//#include <malloc.h>
+//#include <memory.h>
+//#include <tchar.h>
+//
+//#include "resource.h"
+//#include <sstream>
+//
+//
+//#define MAX_LOADSTRING 100
+//
+//char debugMsg[256];
+//void log() {
+//    OutputDebugStringA(debugMsg);
+//}
+//
+//// Global Variables:
+//HINSTANCE hInst;                                // current instance
+//WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+//WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+//
+//// Forward declarations of functions included in this code module:
+//ATOM                MyRegisterClass(HINSTANCE hInstance);
+//BOOL                InitInstance(HINSTANCE, int);
+//LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+//INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+//
+//int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+//                     _In_opt_ HINSTANCE hPrevInstance,    // 永遠是null
+//                     _In_ LPWSTR    lpCmdLine, // Command line 參數
+//                     _In_ int       nCmdShow) 
+//{
+//    UNREFERENCED_PARAMETER(hPrevInstance);
+//    UNREFERENCED_PARAMETER(lpCmdLine);
+//
+//    // TODO: Place code here.
+//
+//    // Initialize global strings
+//    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+//    LoadStringW(hInstance, IDC_HELLODX, szWindowClass, MAX_LOADSTRING);
+//    MyRegisterClass(hInstance);
+//
+//    // Perform application initialization:
+//    if (!InitInstance (hInstance, nCmdShow))
+//    {
+//        return FALSE;
+//    }
+//
+//    //HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_HELLODX));
+//
+//    MSG msg;
+//
+//    // Main message loop:
+//    while (GetMessage(&msg, nullptr, 0, 0))
+//    {
+//        //if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+//        //{
+//            TranslateMessage(&msg);
+//            DispatchMessage(&msg);
+//        //}
+//    }
+//
+//    return (int) msg.wParam;
+//}
+//
+//
+//ATOM MyRegisterClass(HINSTANCE hInstance)
+//{
+//    WNDCLASSEXW wcex;
+//
+//    wcex.cbSize = sizeof(WNDCLASSEX);
+//
+//    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+//    wcex.lpfnWndProc    = WndProc;    // Windows的listener
+//    wcex.cbClsExtra     = 0;
+//    wcex.cbWndExtra     = 0;
+//    wcex.hInstance      = hInstance;
+//    wcex.hIcon          = nullptr;
+//    wcex.hCursor        = nullptr;
+//    wcex.hbrBackground  = nullptr;
+//    wcex.lpszMenuName   = nullptr;
+//    wcex.lpszClassName  = szWindowClass;
+//    wcex.hIconSm        = nullptr;
+//
+//    return RegisterClassExW(&wcex);
+//}
+//
+//
+//BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+//{
+//   hInst = hInstance; // Store instance handle in our global variable
+//
+//   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+//      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+//
+//   if (!hWnd)
+//   {
+//      return FALSE;
+//   }
+//
+//   ShowWindow(hWnd, nCmdShow);
+//   UpdateWindow(hWnd);
+//
+//   return TRUE;
+//}
+//
+//// GetMessage() -> DispatchMessage() -> Event driven 的 Message queue 的 handler (WndProc) -> Default Handler
+//LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+//{
+//    switch (message)
+//    {
+//    case WM_NCMOUSEMOVE:
+//        OutputDebugString(TEXT("NC => 工作列的區域\n"));
+//        break;
+//    case WM_KEYUP:
+//        sprintf_s(debugMsg, "Keyup: %lld\n", wParam);
+//        log();
+//        SetWindowTextA(hWnd, "After Key Pressed");
+//        break;
+//    case WM_CLOSE:
+//        PostQuitMessage(69);
+//        DestroyWindow(hWnd);
+//        break;
+//    case WM_COMMAND:
+//        {
+//            int wmId = LOWORD(wParam);
+//            // Parse the menu selections:
+//            switch (wmId)
+//            {
+//            case IDM_ABOUT:
+//                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+//                break;
+//            case IDM_EXIT:
+//                PostQuitMessage(66);
+//                DestroyWindow(hWnd);
+//                break;
+//            default:
+//                return DefWindowProc(hWnd, message, wParam, lParam);
+//            }
+//        }
+//        break;
+//    case WM_PAINT:
+//        {
+//            PAINTSTRUCT ps;
+//            HDC hdc = BeginPaint(hWnd, &ps);
+//            // TODO: Add any drawing code that uses hdc here...
+//            EndPaint(hWnd, &ps);
+//        }
+//        break;
+//    case WM_DESTROY:
+//        PostQuitMessage(0);
+//        break;
+//    default: // 預設丟給default handler
+//        return DefWindowProc(hWnd, message, wParam, lParam);
+//    }
+//    return 0;
+//}
+//
+//// Message handler for about box.
+//INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+//{
+//    UNREFERENCED_PARAMETER(lParam);
+//    switch (message)
+//    {
+//    case WM_INITDIALOG:
+//        return (INT_PTR)TRUE;
+//
+//    case WM_COMMAND:
+//        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+//        {
+//            EndDialog(hDlg, LOWORD(wParam));
+//            return (INT_PTR)TRUE;
+//        }
+//        break;
+//    }
+//    return (INT_PTR)FALSE;
+//}
